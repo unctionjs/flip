b79c7efc752fcd043d1bbd4bd834054efc0c5d83
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import flip from "./source.js"
+import flattenTree from "./source"
 
 const key = (name) => (iterable) => iterable[name]
 
