9ceb3a670307436dfa32fbeeeae5bb374996e64c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export default function flip (unction: any => any): Function {
-  return function flipUnction (left: any): Function {
-    return function flipUnctionValue (right: any): any {
-      return unction(right)(left)
-    }
-  }
+export default function flip(unction) {
+  return function flipUnction(left) {
+    return function flipUnctionValue(right) {
+      return unction(right)(left);
+    };
+  };
 }
